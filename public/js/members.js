ac5f09f965dbd3ee89917e24724c99d56aa2dfba
David Roman <david_roman09@yahoo.com>
2021-02-21 21:15:02 -0600

    user can add a new post

--- public/js/members.js
+++ public/js/members.js
@@ -1,6 +1,120 @@
 $(document).ready(() => {
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
+
+  var bodyInput = $("#body");
+  var titleInput = $("#title");
+  var cmsForm = $("#cms");
+  var postCategorySelect = $("#list-category");
+  var blogContainer = $(".blog-container");
+  var post;
+
+  function getPost(category){ 
+    var string = category || "";
+    if(string){
+      string = "/category/" + string
+    }
+    $.get("/api/posts" + string, data => {
+      console.log("post ", data)
+      post = data
+      if(!post || !post.length){
+        displayEmpty();
+      } else {
+        initialize();
+      }
+    })
+  }
+
+  getPost()
+
+  function initialize(){
+    blogContainer.empty();
+    var postAdd = [];
+    for(var i = 0; i < post.length; i++){
+      postAdd.push(createPost(post[i]))
+    }
+    blogContainer.append(postAdd);
+  }
+
+  function createPost(post){
+    const postCard = $("<div>");
+    postCard.addClass("card");
+    const postCardHeading = $("<div>");
+    postCardHeading.addClass("card-header");
+    const deleteBtn = $("<button>");
+    deleteBtn.text("delete");
+    deleteBtn.addClass("delete btn btn-danger");
+    const editBtn = $("<button>");
+    editBtn.text("Edit");
+    editBtn.addClass("edit btn btn-default");
+    const postTitle = $("<h2>");
+    const postDate = $("<small>");
+    const postCatogory = $("<h5>");
+    postCatogory.text(post.category);
+    postCatogory.css({
+      float: "right",
+      "font-weight": "700",
+      "margin-top":
+      "-15px"
+    });
+    const postCardBody = $("<div>")
+    postCardBody.addClass("card-body");
+    const postBody = $("<p>");
+    postTitle.text(post.title + "");
+    postBody.text(post.body)
+    const formatDate = new Date(post.createdAt).toLocaleDateString();
+    postDate.text(formatDate);
+    postTitle.append(postDate);
+    postCardHeading.append(deleteBtn);
+    postCardHeading.append(editBtn);
+    postCardHeading.append(postTitle);
+    postCardHeading.append(postCatogory);
+    postCardBody.append(postBody);
+    postCard.append(postCardHeading);
+    postCard.append(postCardBody);
+    postCard.data("post", post);
+    return postCard
+  }
+
+
+  function displayEmpty() {
+    blogContainer.empty();
+    var messageH2 = $("<h2>");
+    messageH2.css({ "text-align": "center", "margin-top": "50px" });
+    messageH2.html("No posts yet for this category, navigate here in order to create a new post.");
+    blogContainer.append(messageH2);
+  }
+
+
+
+  
+  postCategorySelect.val("Personal");
+
+  $(cmsForm).on("submit", function handleFormSubmit(event) {
+    event.preventDefault();
+  
+    if (!titleInput.val().trim() || !bodyInput.val().trim()) {
+      return;
+    }
+    
+    var newPost = {
+      title: titleInput.val().trim(),
+      body: bodyInput.val().trim(),
+      category: postCategorySelect.val()
+    };
+
+    console.log(newPost);
+    submitPost(newPost)
+
+  });
+
+  function submitPost(Post) {
+    $.post("/api/posts/", Post, function() {
+      window.location.href = "/members";
+    });
+  }
+
+
   $.get("/api/user_data").then(data => {
     $(".member-name").text(data.email);
     var name = data.email;
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -50,4 +50,35 @@ module.exports = function(app) {
       });
     }
   });
+
+  app.get("/api/posts/", function(req, res) {
+    db.Post.findAll({})
+      .then(function(dbPost) {
+        res.json(dbPost);
+      });
+  });
+
+  app.get("/api/posts/category/:category", function(req, res) {
+    db.Post.findAll({
+      where: {
+        category: req.params.category
+      }
+    })
+      .then(function(dbPost) {
+        res.json(dbPost);
+      });
+  });
+
+  app.post("/api/posts", function(req, res) {
+    console.log(req.body);
+    db.Post.create({
+      title: req.body.title,
+      body: req.body.body,
+      category: req.body.category
+    })
+      .then(function(dbPost) {
+        res.json(dbPost);
+      });
+  });
+
 };
