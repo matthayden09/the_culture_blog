f9cc1323e9b368dcf7c6f66b221f3b89bce9569b
David Roman <david_roman09@yahoo.com>
2021-02-27 13:13:42 -0600

    heorku connect test 1

--- public/js/members.js
+++ public/js/members.js
@@ -224,7 +224,7 @@ $(document).ready(() => {
     var name = data.email;
 
     // chat 
-    const socket = io.connect("http://localhost:8080", { transport : ['websocket'] });
+    const socket = io.connect("http://localhost:8080", "https://gentle-cliffs-54160.herokuapp.com/");
     const messageContainer = document.getElementById("message-container")
     const messageForm = document.getElementById("send-container")
     const messageInput = document.getElementById("message-input")
