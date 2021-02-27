1f25ca6ec0c4ebc6e2d912c7bd450820fd141043
David Roman <david_roman09@yahoo.com>
2021-02-27 14:24:46 -0600

    heroku edit

--- public/js/members.js
+++ public/js/members.js
@@ -224,7 +224,7 @@ $(document).ready(() => {
     var name = data.email;
 
     // chat 
-    const socket = io.connect("http://localhost:8080");
+    const socket = io.connect("https://gentle-cliffs-54160.herokuapp.com/");
     const messageContainer = document.getElementById("message-container")
     const messageForm = document.getElementById("send-container")
     const messageInput = document.getElementById("message-input")
--- server.js
+++ server.js
@@ -4,12 +4,7 @@ const exphbs = require("express-handlebars");
 const session = require("express-session");
 const app = express();
 const http = require("http").Server(app)
-const io = require("socket.io")(http, {
-  cors: {
-      origin: "http://localhost:8080",
-      methods: ["GET", "POST"]
-  }
-})
+const io = require("socket.io")(http)
 
 // Requiring passport as we've configured it
 const passport = require("./config/passport");
