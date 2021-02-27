92c35464297d33f89ea580e5eb028a9f7970272e
David Roman <david_roman09@yahoo.com>
2021-02-27 14:05:53 -0600

    CORS added

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ const exphbs = require("express-handlebars");
 const session = require("express-session");
 const app = express();
 const http = require("http").Server(app)
-const io = require("socket.io")(http)
+const io = require("socket.io")(http, {
+  cors: {
+      origin: "http://localhost:8080",
+      methods: ["GET", "POST"]
+  }
+})
 
 // Requiring passport as we've configured it
 const passport = require("./config/passport");
